3eca6d0bf7469385f31e2aa48851d2574cf38b8e
hwangyu.kim <skykhk01@gmail.com>
2023-06-01 15:35:54 +0900

    [feature] 44p

--- Sample-Test1/PrimeFactor_test.cpp
+++ Sample-Test1/PrimeFactor_test.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class PrimeFixture: public testing::Test
+class PrimeFixture : public testing::Test
 {
 public:
 	PrimeFactor prime_factor;
@@ -18,12 +18,18 @@ TEST_F(PrimeFixture, Of1)
 
 TEST_F(PrimeFixture, Of2)
 {
-	expected = {2};
+	expected = { 2 };
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
 
 TEST_F(PrimeFixture, Of3)
 {
-	expected = {3};
+	expected = { 3 };
 	EXPECT_EQ(expected, prime_factor.of(3));
+}
+
+TEST_F(PrimeFixture, Of4)
+{
+	expected = { 2,2 };
+	EXPECT_EQ(expected, prime_factor.of(4));
 }
